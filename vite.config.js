5c1a54668f0ecc5a0c3136f775115724d5924062
Tobi Badiru <tobibadiru@outlook.com>
2025-09-29 20:12:58 -0400

    Fix Render Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+      allowedHosts: [
+        'comp229-f25-401-assignment1.onrender.com'
+      ]
+  }
 })
